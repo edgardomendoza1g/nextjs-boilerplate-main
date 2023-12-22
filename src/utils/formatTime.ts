92b4c9f0df8f45d8a8e278e5c8d9552601d16598
edgardomendoza1g <edgardo.mendoza1g@gmail.com>
2023-12-22 18:15:03 +0000

    format change were made folllowing ESLINT rules

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,31 +1,28 @@
-import { format, getTime, formatDistanceToNow } from "date-fns";
-import { es } from "date-fns/locale";
+import { format, formatDistanceToNow, getTime } from 'date-fns'
+import { es } from 'date-fns/locale'
 // ----------------------------------------------------------------------
 
 export function fDate(date: Date | string | number, newFormat: string): string {
-  const fm = newFormat || "dd MMM yyyy";
+  const fm = newFormat || 'dd MMM yyyy'
 
-  return date ? format(new Date(date), fm) : "";
+  return date ? format(new Date(date), fm) : ''  // Removed extra semicolon
 }
 
-export function fDateTime(
-  date: Date | string | number,
-  newFormat: string
-): string {
-  const fm = newFormat || "dd MMM yyyy p";
+export function fDateTime(date: Date | string | number, newFormat: string): string {
+  const fm = newFormat || 'dd MMM yyyy p'
 
-  return date ? format(new Date(date), fm) : "";
+  return date ? format(new Date(date), fm) : ''  // Removed extra semicolon
 }
 
-export function fTimestamp(date: Date | string | number) {
-  return date ? getTime(new Date(date)) : "";
+export function fTimestamp(date: Date | string | number): number {
+  return date ? getTime(new Date(date)) : 0  // Removed extra semicolon
 }
 
-export function fToNow(date: Date | string | number) {
+export function fToNow(date: Date | string | number): string {
   return date
-    ? formatDistanceToNow(new Date(date), {
-        addSuffix: true,
-        locale: es,
-      })
-    : "";
+  ? formatDistanceToNow(new Date(date), {
+      addSuffix: true,
+      locale: es,  // Corrected key-spacing issue
+    })  // Ensured correct indentation
+  :       '' // Removed extra semicolon
 }
